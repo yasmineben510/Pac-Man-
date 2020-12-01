3799d03f5e3ce24f4391416aec7544548eafb7c2
saraanej <sara.anejjar@epfl.ch>
2020-12-01 23:02:07 +0100

    0

--- CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/SuperPacman.java
+++ CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/SuperPacman.java
@@ -49,7 +49,7 @@ public class SuperPacman extends RPG {
 		if (super.begin(window, fileSystem)) {
 
 			createAreas();
-			areaIndex = 2;
+			areaIndex = 0;
 			Area area = setCurrentArea(areas[areaIndex], true);
 			player = new SuperPacmanPlayer(area, Orientation.RIGHT, ((SuperPacmanArea)area).getPlayerSpawnPosition(),"superpacman/bonus");
 			initPlayer(player);
