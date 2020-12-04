1910b28c5b1b36cd18af7f18509767299c9f9737
saraanej <sara.anejjar@epfl.ch>
2020-12-04 18:09:22 +0100

    gui

--- CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/SuperPacmanPlayerStatusGUI.java
+++ CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/SuperPacmanPlayerStatusGUI.java
@@ -1,17 +1,38 @@
 package ch.epfl.cs107.play.game.superpacman.actor;
 
 import ch.epfl.cs107.play.game.actor.Graphics;
+import ch.epfl.cs107.play.game.actor.ImageGraphics;
+import ch.epfl.cs107.play.game.areagame.io.ResourcePath;
+import ch.epfl.cs107.play.math.RegionOfInterest;
+import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Canvas;
 
 public class SuperPacmanPlayerStatusGUI implements Graphics{
-
 	
+	
+
+	public SuperPacmanPlayerStatusGUI() {
+		
+		
+		
+		
+	}
 	
 	
 	
 	@Override
 	public void draw(Canvas canvas) {
-		// TODO Auto-generated method stub
+		
+		float width = canvas.getScaledWidth();
+		float height = canvas.getScaledHeight();
+		Vector anchor = canvas.getTransform().getOrigin()
+		.sub(new Vector(width/2, height/2));
+		
+		ImageGraphics life = new
+		ImageGraphics(ResourcePath.getSprite("superpacman/lifeDisplay"),
+				      1.f, 1.f, new RegionOfInterest(m, 0, 64, 64),
+				      anchor.add(new Vector(n, height - 1.375f)), 1, DEPTH);
+		life.draw(canvas);
 		
 	}
 
