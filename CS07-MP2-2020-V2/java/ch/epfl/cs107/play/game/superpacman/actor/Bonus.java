897d005bf6202876371da31cd88a24b5fd8132a3
yasmineben510 <yasmin.benrahhal@epfl.chh>
2020-12-05 17:03:48 +0100

    collectable

--- CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/Bonus.java
+++ CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/Bonus.java
@@ -1,16 +1,61 @@
 package ch.epfl.cs107.play.game.superpacman.actor;
 
-import ch.epfl.cs107.play.game.areagame.actor.CollectableAreaEntity;
-import ch.epfl.cs107.play.game.areagame.actor.Sprite;
-import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
-import ch.epfl.cs107.play.math.Positionable;
+import java.util.List;
 
-public class Bonus implements CollectableAreaEntity{
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.AreaEntity;
+import ch.epfl.cs107.play.game.areagame.actor.CollectableAreaEntity;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Positionable;
+import ch.epfl.cs107.play.window.Canvas;
+
+public class Bonus extends AreaEntity implements CollectableAreaEntity{
 	
 	Sprite[] sprites;
 	
-	public Bonus(Positionable parent) {
-		sprites = RPGSprite.extractSprites("superpacman/coin", 4, 1, 1, parent , 16, 16);
+	public Bonus(Area area, Orientation orientation, DiscreteCoordinates position) {
+		super(area,orientation,position);
+		sprites = RPGSprite.extractSprites("superpacman/coin", 4, 1, 1, this , 16, 16);
+	}
+
+	@Override
+	public List<DiscreteCoordinates> getCurrentCells() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean takeCellSpace() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCellInteractable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isViewInteractable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void acceptInteraction(AreaInteractionVisitor v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/Diamond.java
+++ CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/Diamond.java
@@ -1,18 +1,63 @@
 package ch.epfl.cs107.play.game.superpacman.actor;
 
-import ch.epfl.cs107.play.game.areagame.actor.CollectableAreaEntity;
-import ch.epfl.cs107.play.game.areagame.actor.Sprite;
-import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
-import ch.epfl.cs107.play.math.Positionable;
+import java.util.List;
 
-public class Diamond implements CollectableAreaEntity{
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.AreaEntity;
+import ch.epfl.cs107.play.game.areagame.actor.CollectableAreaEntity;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Positionable;
+import ch.epfl.cs107.play.window.Canvas;
+
+public class Diamond extends AreaEntity implements CollectableAreaEntity{
 	
 	final int points = 10;
 	
-    Sprite[] sprites;
+    Sprite sprites;
 	
-	public Diamond(Positionable parent) {
-		sprites = RPGSprite.extractSprites("superpacman/diamond", 4, 1, 1, parent , 16, 16);
+	public Diamond(Area area, Orientation orientation, DiscreteCoordinates position) {
+		super(area,orientation,position);
+		//sprites = RPGSprite.extractSprites("superpacman/diamond", 4, 1, 1, this , 16, 16);
+	}
+
+	@Override
+	public List<DiscreteCoordinates> getCurrentCells() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean takeCellSpace() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCellInteractable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isViewInteractable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void acceptInteraction(AreaInteractionVisitor v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
