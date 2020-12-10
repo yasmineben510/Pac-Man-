697d0e91fda3f1e515a6cbf9c463d21ec2c60db9
saraanej <sara.anejjar@epfl.ch>
2020-12-10 14:48:48 +0100

    g

--- CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/Ghost.java
+++ CS07-MP2-2020-V2/java/ch/epfl/cs107/play/game/superpacman/actor/Ghost.java
@@ -1,44 +1,44 @@
 package ch.epfl.cs107.play.game.superpacman.actor;
 
+import java.util.Collections;
 import java.util.List;
 
 import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Interactable;
+import ch.epfl.cs107.play.game.areagame.actor.Interactor;
 import ch.epfl.cs107.play.game.areagame.actor.MovableAreaEntity;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.window.Canvas;
 
-public class Ghost extends MovableAreaEntity{
+public class Ghost extends MovableAreaEntity implements Interactor{
 	
-	private final int GHOST_SCORE = 500;
+	private final int GHOST_SCORE = 500; // static or not ? 
 
-	public Ghost(Area area, Orientation orientation, DiscreteCoordinates position) {
+	public Ghost(Area area, Orientation orientation, DiscreteCoordinates position, DiscreteCoordinates shelter) {
 		super(area, orientation, position);
 		// TODO Auto-generated constructor stub
 	}
 
+	/// implements Interactable
 	@Override
 	public List<DiscreteCoordinates> getCurrentCells() {
-		// TODO Auto-generated method stub
-		return null;
+		return Collections.singletonList(getCurrentMainCellCoordinates());
 	}
 
 	@Override
 	public boolean takeCellSpace() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isCellInteractable() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isViewInteractable() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -50,6 +50,29 @@ public class Ghost extends MovableAreaEntity{
 
 	@Override
 	public void draw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/// implements Interactor
+	@Override
+	public List<DiscreteCoordinates> getFieldOfViewCells() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean wantsCellInteraction() {
+		return false;
+	}
+
+	@Override
+	public boolean wantsViewInteraction() {
+		return true;
+	}
+
+	@Override
+	public void interactWith(Interactable other) {
 		// TODO Auto-generated method stub
 		
 	}
